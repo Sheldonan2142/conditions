7e86fcae58f4540b67c8300cd7c05bd4d741b43d
Sheldonan2142 <56047116+Sheldonan2142@users.noreply.github.com>
2019-10-01 12:50:35 -0700

    start file

---
+++ conditions.py
@@ -0,0 +1,29 @@
+# conditional
+print("welcome all u ho's")
+age = int(input("what's ya' age huh?"))
+
+if age > 17: 
+	print("you can see all those spicy movies")
+else:
+	print("ew a child get out")
+
+print("have good day")
+
+myNumber = 4
+choice = int(input("pick a spicy number between 1-10 ;)"))
+if myNumber == choice:
+	print("oh what a nerd?? you got it right but whatev")
+elif choice < myNumber:
+	print("ha NOPE")
+else:
+	print("too high, just like me")
+
+# == (equal to), <, >, <=, >=, != (not equal to)
+
+bDay =input("its ya' birthday?? :O ( yes / no ):")
+if bDay == "yes" or bDay == "you know it ho":
+	print("HAPPY BIRTHDAY YEEHAW")
+elif  bDay == "no":
+	print("get older then")
+else:
+	print("answer the question or i'll kill you")
